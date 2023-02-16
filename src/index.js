7824aa092d48a0994f2d05e082d44fe51da74078
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2023-02-16 09:04:45 +0300

    done and dusted

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import { applyMiddleware } from "redux";
 import { createStore } from "redux";
 import  thunkMiddleware  from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import rootReducer from "./reducer";
 
-const store = createStore(
-  rootReducer, applyMiddleware(thunkMiddleware));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(rootReducer, composedEnhancer);
+
 
 ReactDOM.render(
   <Provider store={store}>
